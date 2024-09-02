6109b0dde692bbc72fdde6ac7d4b0acb729a2252
antoniahp <a.herrerapuertas@gmail.com>
2024-09-02 12:21:30 +0200

    utc-0 to spain timezone

--- app/events/infraestructure/telegram_live_streaming_notifier.py
+++ app/events/infraestructure/telegram_live_streaming_notifier.py
@@ -1,6 +1,10 @@
+import pytz
+
 from events.domain.event import Event
 from events.domain.live_streaming_notifier import LiveStreamingNotifier
 import telegram as _telegram
+
+
 
 
 class TelegramLiveStreamingNotifier(LiveStreamingNotifier):
@@ -9,8 +13,10 @@ class TelegramLiveStreamingNotifier(LiveStreamingNotifier):
         self.__chat_id = chat_id
 
     def notify(self, event: Event) -> None:
+        spain_tz = pytz.timezone('Europe/Madrid')
+        local_time = event.date.astimezone(spain_tz)
         live_streaming_url = f"https://geek.live/live/{event.provider_id}"
-        caption = f"A las *{event.date.strftime('%H:%M')}* se realizará el evento de'{event.title}'\n *Categoría:* {event.get_category()} \n [Ver directo]({live_streaming_url})"
+        caption = f"A las *{local_time.strftime('%H:%M')}* se realizará el evento de '{event.title}'\n*Categoría:* {event.get_category()} \n [Ver directo]({live_streaming_url})"
         self.__bot.send_photo(
             chat_id=self.__chat_id,
             photo=event.image,
